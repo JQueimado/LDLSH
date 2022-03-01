6a25d61a5f4e50d59c71d00bc08b6be2d03cc4e7
JQueimado <jpqueimado@gmail.com>
2022-03-01 14:05:49 +0000

    Minor changes

--- LDLSH/src/main/java/SystemLayer/ProcessInterfacesses/DistanceMeasurer.java
+++ LDLSH/src/main/java/SystemLayer/ProcessInterfacesses/DistanceMeasurer.java
@@ -1,4 +1,9 @@
 package SystemLayer.ProcessInterfacesses;
 
+import SystemLayer.Data.DataObject;
+
+import javax.xml.crypto.Data;
+
 public interface DistanceMeasurer {
+    int getDistance(DataObject object_a, DataObject object_b);
 }
