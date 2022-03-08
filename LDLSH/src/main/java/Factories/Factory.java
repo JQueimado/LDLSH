0149ac5388c7dc6363eb903856a7c461a407ec0e
JQueimado <jpqueimado@gmail.com>
2022-03-08 17:52:00 +0000

    Add ConfigException

--- LDLSH/src/main/java/Factories/Factory.java
+++ LDLSH/src/main/java/Factories/Factory.java
@@ -1,4 +1,11 @@
 package Factories;
 
 public interface Factory {
+    class ConfigException extends Exception{
+        String config_name;
+        String config_value;
+        public ConfigException(String msg, String config_name, String config_value){
+            super(msg);
+        }
+    }
 }
