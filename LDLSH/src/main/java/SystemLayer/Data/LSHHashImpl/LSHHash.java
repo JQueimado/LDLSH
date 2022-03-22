9e973ad1c424fe65dffc0bc82b92dfb2df2029e2
JQueimado <jpqueimado@gmail.com>
2022-03-22 17:52:54 +0000

    Clean LSHHash

--- LDLSH/src/main/java/SystemLayer/Data/LSHHashImpl/LSHHash.java
+++ LDLSH/src/main/java/SystemLayer/Data/LSHHashImpl/LSHHash.java
@@ -3,8 +3,6 @@ package SystemLayer.Data.LSHHashImpl;
 import SystemLayer.Data.DataObjectsImpl.DataObject;
 
 import java.io.Serializable;
-import java.lang.reflect.Array;
-import java.util.Set;
 
 public interface LSHHash extends Serializable {
     void setObject( DataObject object );
