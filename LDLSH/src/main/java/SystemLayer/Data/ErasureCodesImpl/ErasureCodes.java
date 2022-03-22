95b66e70d9ca9ca1c2121023a05364b5143cab3e
JQueimado <jpqueimado@gmail.com>
2022-03-22 17:52:38 +0000

    Clean LSHHash

--- LDLSH/src/main/java/SystemLayer/Data/ErasureCodesImpl/ErasureCodes.java
+++ LDLSH/src/main/java/SystemLayer/Data/ErasureCodesImpl/ErasureCodes.java
@@ -1,7 +1,11 @@
 package SystemLayer.Data.ErasureCodesImpl;
 
+import SystemLayer.Data.DataObjectsImpl.DataObject;
+
 public interface ErasureCodes {
-    void createErasureCodes();
+    void encodeDataObject( DataObject dataObject );
+    DataObject decodeDataObject();
+
     byte[] getErasureCodes();
     byte[][] getErasureBlocks();
 }
