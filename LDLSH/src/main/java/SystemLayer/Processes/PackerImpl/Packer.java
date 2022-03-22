5a4026aef0217d01a5f4afa3d042a640a1717e4c
JQueimado <jpqueimado@gmail.com>
2022-03-22 12:18:17 +0000

    Adapt project to previous changes

--- LDLSH/src/main/java/SystemLayer/Processes/PackerImpl/Packer.java
+++ LDLSH/src/main/java/SystemLayer/Processes/PackerImpl/Packer.java
@@ -2,7 +2,7 @@ package SystemLayer.Processes.PackerImpl;
 
 import SystemLayer.Data.ErasureCodesImpl.ErasureCodes;
 import SystemLayer.Data.LSHHashImpl.LSHHash;
-import SystemLayer.Data.UniqueIdentifier;
+import SystemLayer.Data.UniqueIndentifierImpl.UniqueIdentifier;
 
 public interface Packer {
     public Package[] pack(UniqueIdentifier objectName, LSHHash lsh, ErasureCodes codes);
--- LDLSH/src/main/java/SystemLayer/Processes/PackerImpl/StandardPacker.java
+++ LDLSH/src/main/java/SystemLayer/Processes/PackerImpl/StandardPacker.java
@@ -2,7 +2,7 @@ package SystemLayer.Processes.PackerImpl;
 
 import SystemLayer.Data.ErasureCodesImpl.ErasureCodes;
 import SystemLayer.Data.LSHHashImpl.LSHHash;
-import SystemLayer.Data.UniqueIdentifier;
+import SystemLayer.Data.UniqueIndentifierImpl.UniqueIdentifier;
 
 public class StandardPacker implements Packer{
     @Override
