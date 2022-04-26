bc94f81e2185f4dbdb4a24efe5671c604f37b70e
JQueimado <jpqueimado@gmail.com>
2022-04-26 18:13:51 +0100

    Override equals and hashCode methods

--- LDLSH/src/main/java/SystemLayer/Data/LSHHashImpl/LSHHash.java
+++ LDLSH/src/main/java/SystemLayer/Data/LSHHashImpl/LSHHash.java
@@ -16,6 +16,20 @@ public interface LSHHash extends Serializable, Comparable<LSHHash> {
         public int compareTo(LSHHashBlock o) {
             return Arrays.compare(lshBlock, o.lshBlock);
         }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj.getClass() != LSHHashBlock.class)
+                return false;
+            LSHHashBlock conv = (LSHHashBlock) obj;
+            return Arrays.equals( this.lshBlock, conv.lshBlock );
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(this.lshBlock);
+        }
+
         /**/
     }
 }
