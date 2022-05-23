aa9e4e36475da941608db265b3c6348a0b082518
JQueimado <jpqueimado@gmail.com>
2022-05-23 11:49:48 +0100

    Override compare and equals properties in ErasureBlocks

--- LDLSH/src/main/java/SystemLayer/Data/ErasureCodesImpl/ErasureCodes.java
+++ LDLSH/src/main/java/SystemLayer/Data/ErasureCodesImpl/ErasureCodes.java
@@ -3,6 +3,7 @@ package SystemLayer.Data.ErasureCodesImpl;
 import SystemLayer.Data.DataObjectsImpl.DataObject;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public interface ErasureCodes extends Serializable, Comparable<ErasureCodes> {
     void encodeDataObject( DataObject dataObject, int n_blocks );
@@ -13,8 +14,20 @@ public interface ErasureCodes extends Serializable, Comparable<ErasureCodes> {
     ErasureBlock[] getErasureBlocks();
     ErasureBlock getBlockAt( int blocks );
 
-    record ErasureBlock( byte[] block_data, int position ){
-        /**/
+    record ErasureBlock( byte[] block_data, int position ) implements Comparable<ErasureBlock> {
+        @Override
+        public int compareTo(ErasureBlock o) {
+            if ( position != o.position )
+                return -1;
+            return Arrays.compare(block_data, o.block_data);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if( obj.getClass() != ErasureBlock.class )
+                return false;
+            return this.compareTo( (ErasureBlock) obj ) == 0 ;
+        }
     }
 
     class IncompleteBlockException extends Exception{
