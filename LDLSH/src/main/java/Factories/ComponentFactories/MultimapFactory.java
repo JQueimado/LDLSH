6e307d025eeb86605780de8812172ac0f9dcbf44
JQueimado <jpqueimado@gmail.com>
2022-07-01 16:28:58 +0100

    Bug fix

--- LDLSH/src/main/java/Factories/ComponentFactories/MultimapFactory.java
+++ LDLSH/src/main/java/Factories/ComponentFactories/MultimapFactory.java
@@ -3,14 +3,17 @@ package Factories.ComponentFactories;
 import Factories.Factory;
 import SystemLayer.Components.MultiMapImpl.GuavaInMemoryMultiMap;
 import SystemLayer.Components.MultiMapImpl.MultiMap;
+import SystemLayer.Components.MultiMapImpl.RemoteMultimap;
 import SystemLayer.Containers.DataContainer;
 
 
 public class MultimapFactory implements Factory {
     private enum configurations {NONE,GUAVA_MEMORY_MULTIMAP, REMOTE_MULTIMAP}
 
-    public MultimapFactory(){
-        /**/
+    private DataContainer appContext;
+
+    public MultimapFactory( DataContainer appContext ){
+        this.appContext = appContext;
     }
 
     public MultiMap getNewMultiMap(String config) throws Exception {
@@ -18,7 +21,11 @@ public class MultimapFactory implements Factory {
         switch ( configuration ){
 
             case GUAVA_MEMORY_MULTIMAP -> {
-                return new GuavaInMemoryMultiMap();
+                return new GuavaInMemoryMultiMap(appContext);
+            }
+
+            case REMOTE_MULTIMAP -> {
+                return new RemoteMultimap(appContext);
             }
 
             default ->{
