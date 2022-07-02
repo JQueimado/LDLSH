bb42d3e6e6d07e104b6ac40bc2171fb68a929b03
JQueimado <jpqueimado@gmail.com>
2022-07-02 16:56:10 +0100

    Fixed issue with multimaps config

--- LDLSH/src/main/java/SystemLayer/Components/SystemServer/SystemImpl.java
+++ LDLSH/src/main/java/SystemLayer/Components/SystemServer/SystemImpl.java
@@ -19,35 +19,93 @@ import java.util.concurrent.Future;
 
 public class SystemImpl implements SystemServer {
 
+    private enum NodeTypes { MULTIMAP_SERVER, CLIENT_WORKER }
+
     private static final String nBands_config = "N_BANDS";
+    private static final String nodeType_config = "NODE_TYPE";
+    private static final String multiMapPosition_config = "MULTIMAP_POSITION";
 
     private final DataContainer context;
 
     public SystemImpl(DataContainer context ) throws Exception {
         this.context = context;
 
-        //Setup
         Configurator configurator = context.getConfigurator();
-        String bands_string = "";
-        int bands = 0;
-        try {
-            bands_string = configurator.getConfig(nBands_config);
-            bands = Integer.parseInt(bands_string);
+
+        String nodeType_value = "";
+        NodeTypes nodeType;
+        try{
+            nodeType_value = configurator.getConfig(nodeType_config);
+            nodeType = NodeTypes.valueOf( nodeType_value );
         }catch (Exception e){
-            UnknownConfigException.handler( new UnknownConfigException(nBands_config, bands_string));
+            UnknownConfigException.handler( new UnknownConfigException(nodeType_config, nodeType_value) );
+            return;
         }
 
-        //-MultiMaps
-        MultimapFactory multimapFactory = new MultimapFactory(context);
-        String multimapConfig = configurator.getConfig("MULTIMAP");
-        MultiMap[] multiMaps = new MultiMap[bands];
-        for ( int i = 0; i<bands; i++ ){
-            MultiMap current = multimapFactory.getNewMultiMap(multimapConfig);
-            current.setHashBlockPosition(i);
-            current.setTotalBlocks(bands);
-            multiMaps[i] = current;
+        switch (nodeType){
+            case MULTIMAP_SERVER -> {
+                //-MultiMaps
+                MultimapFactory multimapFactory = new MultimapFactory(context); //Create factory
+                String multimapConfig = configurator.getConfig("MULTIMAP"); //Get multimap Config
+
+                //Get Bands
+                String bands_string = "";
+                int bands = 0;
+                try {
+                    bands_string = configurator.getConfig(nBands_config);
+                    bands = Integer.parseInt(bands_string);
+                }catch (Exception e){
+                    UnknownConfigException.handler( new UnknownConfigException(nBands_config, bands_string));
+                }
+
+                //Get Map position
+                String multiMapPosition_value = "";
+                int multiMapPosition;
+                try {
+                    multiMapPosition_value = configurator.getConfig(multiMapPosition_config);
+                    multiMapPosition = Integer.parseInt(multiMapPosition_value);
+                }catch (Exception e){
+                    UnknownConfigException.handler(
+                            new UnknownConfigException(multiMapPosition_config, multiMapPosition_value)
+                    );
+                    return;
+                }
+
+                MultiMap[] multiMaps = new MultiMap[1]; //Set array to size 1
+
+                MultiMap current = multimapFactory.getNewMultiMap(multimapConfig); //get new Multimap
+                current.setHashBlockPosition(multiMapPosition);
+                current.setTotalBlocks(bands);
+
+                multiMaps[0] = current;
+                context.setMultiMaps(multiMaps);
+            }
+
+            case CLIENT_WORKER -> {
+                //-MultiMaps
+                MultimapFactory multimapFactory = new MultimapFactory(context); //Create factory
+                String multimapConfig = configurator.getConfig("MULTIMAP"); //Get multimap Config
+
+                //Get Bands
+                String bands_string = "";
+                int bands = 0;
+                try {
+                    bands_string = configurator.getConfig(nBands_config);
+                    bands = Integer.parseInt(bands_string);
+                }catch (Exception e){
+                    UnknownConfigException.handler( new UnknownConfigException(nBands_config, bands_string));
+                }
+
+                MultiMap[] multiMaps = new MultiMap[bands];
+                for ( int i = 0; i<bands; i++ ){
+                    MultiMap current = multimapFactory.getNewMultiMap(multimapConfig);
+                    current.setHashBlockPosition(i);
+                    current.setTotalBlocks(bands);
+                    multiMaps[i] = current;
+                }
+                context.setMultiMaps(multiMaps);
+            }
         }
-        context.setMultiMaps(multiMaps);
 
         //-Communication
         context.getCommunicationLayer();
