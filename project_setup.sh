4a00bfe5857d36557d4680f001c5d7f011af2701
JQueimado <jpqueimado@gmail.com>
2022-07-24 15:39:46 +0100

    add  --no-check-certificate to wget

--- project_setup.sh
+++ project_setup.sh
@@ -1,5 +1,5 @@
 #java 17
-wget https://download.oracle.com/java/17/archive/jdk-17.0.4_linux-x64_bin.deb
+wget https://download.oracle.com/java/17/archive/jdk-17.0.4_linux-x64_bin.deb --no-check-certificate
 sudo dpkg -i jdk-17.0.4_linux-x64_bin.deb
 sudo apt-get -f install -y
 sudo update-alternatives  --install /usr/bin/java java /usr/lib/jvm/jdk-17/bin/java 1
@@ -9,7 +9,7 @@ sudo update-alternatives --set javac /usr/lib/jvm/jdk-17/bin/javac
 export JAVA_HOME=/usr/lib/jvm/jdk-17
 
 #maven
-wget https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
+wget https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz --no-check-certificate
 tar xzvf apache-maven-3.8.6-bin.tar.gz
 sudo mkdir /usr/lib/maven/
 sudo mkdir /usr/lib/maven/apache-maven-3.8.6
