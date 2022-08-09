02a60a1743169025968bd38778105b5eebb02e75
JQueimado <jpqueimado@gmail.com>
2022-08-09 17:12:10 +0100

    add run- tests

--- run-tests.sh
+++ run-tests.sh
@@ -6,18 +6,28 @@ run_java(){
     DIRCONFIG=$1
     OP=$2
     DATAFILE=$3
-    nohup java -server -Xmx100g -XX:+UseG1GC -jar LDLSH-3.2.jar $DIRCONFIG/ClientNode-$HOST.quinta.properties $OP $DATAFILE
+    RESULTS=$4
+    TIMESTAMP=`date +%s` 
+    java -server -Xmx100g -XX:+UseG1GC -jar LDLSH-3.2.jar $DIRCONFIG/ClientNode.properties "-${OP}" $DATAFILE >> ${RESULTS}/test_${OP}_${DATAFILE}_${TIMESTAMP}.txt
 }
 
 LDLSH_Accuracy_Test(){
     ITERATIONS=$1
-    DATAFILE=$2
+    INSERTFILE=$2
+    QUERYFILE=$3
+    CONFIGFILE="{$CONFIGSFOLDER}/LDLSH_Quinta"
+    RESULTSFOLDER="LDLSH_ACCURACY_TESTS"
 
+    #Setup
     ./run_quinta_server all --change_branch Test-Accuracy
     ./run_quinta_server all -js
 
+    run_java $CONFIGFILE "i" $INSERTFILE $RESULTSFOLDER
+
     for IT in 1 .. $ITERATIONS
     do
-        run_java "{$CONFIGSFOLDER}/Demo"
+        run_java $CONFIGFILE "q" $QUERYFILE $RESULTSFOLDER
     done
 }
+
+LDLSH_Accuracy_Test $1 $2 $3
