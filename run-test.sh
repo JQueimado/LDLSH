65c2ec5ee77ef03456b6d0b462de07ab8b91877b
JQueimado <jpqueimado@gmail.com>
2022-08-10 17:59:34 +0100

    fix issue with argument number

--- run-test.sh
+++ run-test.sh
@@ -1,13 +1,13 @@
-if [ $# -eq 3 ];
+if ! [ $# -eq 4 ];
 then
-    DIRCONFIG=$1
-    OP=$2
-    DATAFILE=$3
-    RESULTSFOLDER=$4
-    TIMESTAMP=$(date +%s)
-    FILENAME="${RESULTSFOLDER}/test_${OP}_${TIMESTAMP}.txt"
-    echo "Test to be stored in ${FILENAME}"
-    java -server -Xmx100g -XX:+UseG1GC -jar LDLSH-3.2.jar "${DIRCONFIG}/ClientNode.properties" "${OP}" "${DATAFILE}" #> "${RESULTSFOLDER}/test_${OP}_${TIMESTAMP}.txt"
-else
-    exit 1
+   exit 1
 fi
+
+DIRCONFIG=$1
+OP=$2
+DATAFILE=$3
+RESULTSFOLDER=$4
+TIMESTAMP=$(date +%s)
+FILENAME="${RESULTSFOLDER}/test_${OP}_${TIMESTAMP}.txt"
+echo "Test to be stored in ${FILENAME}"
+java -server -Xmx100g -XX:+UseG1GC -jar LDLSH-3.2.jar "${DIRCONFIG}/ClientNode.properties" "${OP}" "${DATAFILE}" #> "${RESULTSFOLDER}/test_${OP}_${TIMESTAMP}.txt"
