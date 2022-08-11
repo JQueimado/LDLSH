9b3c2f0ed0b261e6c08b20fee7a2484e70216a52
JQueimado <jpqueimado@gmail.com>
2022-08-11 01:12:49 +0100

    debug

--- run-test.sh
+++ run-test.sh
@@ -9,5 +9,5 @@ DATAFILE=$3
 RESULTSFOLDER=$4
 TIMESTAMP=$(date +%s)
 FILENAME="${RESULTSFOLDER}/test_${OP}_${TIMESTAMP}.txt"
-echo "Test to be stored in ${FILENAME}"
+echo "${DIRCONFIG} ${OP} ${DATAFILE} ${FILENAME}"
 $(java -server -Xmx100g -XX:+UseG1GC -jar LDLSH-3.2.jar "${DIRCONFIG}/ClientNode.properties" "-${OP}" "${DATAFILE}") >> "${FILENAME}"
