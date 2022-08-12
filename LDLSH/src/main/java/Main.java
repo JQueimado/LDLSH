f6be105918e8c5061ee5cef6d33677b2efdb7ae0
JQueimado <jpqueimado@gmail.com>
2022-08-12 20:46:53 +0100

    fix executors shutdown before task completion

--- LDLSH/src/main/java/Main.java
+++ LDLSH/src/main/java/Main.java
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
@@ -41,6 +43,8 @@ public class Main {
                 data.add( dataObject );
             }
 
+        AtomicInteger successCounter = new AtomicInteger();
+
         //Execute
         switch (op) {
             //Insert File
@@ -53,6 +57,7 @@ public class Main {
                         @Override
                         public void onSuccess(DataObject object) {
                             //Nothing
+                            successCounter.getAndIncrement();
                         }
 
                         @Override
@@ -62,10 +67,6 @@ public class Main {
                         }
                     }, dataContainer.getCallbackExecutor());
                 }
-                //System.out.println("done");
-                dataContainer.getExecutorService().shutdown(); //waits all tasks termination
-                //assert i[0] == operations;
-                System.exit(0);
             }
 
             case "-q" -> {
@@ -78,6 +79,7 @@ public class Main {
                         final DataObject<?> e = dataElement;
                         @Override
                         public void onSuccess(DataObject object) {
+                            successCounter.getAndIncrement();
                         }
 
                         @Override
@@ -86,10 +88,28 @@ public class Main {
                         }
                     }, dataContainer.getCallbackExecutor());
                 }
-                dataContainer.getExecutorService().shutdown(); //waits all tasks termination
-                //assert i[0] == operations;
-                System.exit(0);
             }
         }
+
+        //Shutdown
+        //End
+        try {
+            if(! dataContainer.getExecutorService().awaitTermination(1, TimeUnit.SECONDS))
+                dataContainer.getExecutorService().shutdownNow();
+        }catch (InterruptedException e){
+            dataContainer.getExecutorService().shutdownNow();
+        }
+
+        try {
+            if(! dataContainer.getCallbackExecutor().awaitTermination(1, TimeUnit.SECONDS))
+                dataContainer.getCallbackExecutor().shutdownNow();
+        }catch (InterruptedException e){
+            dataContainer.getCallbackExecutor().shutdownNow();
+        }
+
+        if (successCounter.get() != data.size() )
+            throw new Exception("Not all Inserts were performed.");
+
+        System.exit(0);
     }
 }
