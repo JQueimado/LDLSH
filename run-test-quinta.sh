768d5d52931c008e8b774a2c9a97a105ea2493c2
JQueimado <jpqueimado@gmail.com>
2022-08-20 15:19:24 +0100

    add multi testing for each test

--- run-test-quinta.sh
+++ run-test-quinta.sh
@@ -2,6 +2,11 @@
 DIR="/root/jqueimado/Large-scale_distributed_similarity_search_with_Locality-Sensitive_Hashing"
 DATASETS="${DIR}/data_sets"
 CONFIGS="${DIR}/LDLSH/Test_Batery"
+CLIENT="t5"
+SERVERS="t6 t7 t8"
+BRANCH="Tests"
+TESTS="accuracy latency throughput"
+
 change_branch(){
 	ssh "$1" "cd ${DIR}; git checkout $2"
 	pulloutput=$(ssh "$1" "cd ${DIR}; git pull;")
@@ -22,12 +27,23 @@ run_server_jar(){
 run_test_client_jar(){
 	#ssh "$1" "cd ${DIR}; ./run-test.sh $2 $3 $4 $5"
 	TIMESTAMP=$(date +%s)
-	FILENAME="$5/test_$3_${TIMESTAMP}.txt"
-	ssh $1 "cd ${DIR}; java -server -Xmx100g -XX:+UseG1GC -jar LDLSH-3.2.jar $2/client_$6.properties -$3 $4 >> ${FILENAME}"
+	FILENAME="$5/test_$6_$3_${TIMESTAMP}.txt"
+	ssh $1 "cd ${DIR}; java -server -Xmx100g -XX:+UseG1GC -Dio.netty.leakDetection.level=disabled -jar LDLSH-3.2.jar $2/client_$6.properties -$3 $4 >> ${FILENAME}"
 }
 
 kill_process(){
 	ssh "$1" "cd ${DIR}; ./run-server.sh -k"
+}
+
+setup_tests(){
+	echo "--- Setup client ${CLIENT} ---"
+	change_branch $CLIENT $BRANCH
+
+	for SERVER in $SERVERS
+	do
+		echo "--- Setup server ${SERVER} ---"
+		change_branch "$SERVER" "$BRANCH"
+	done
 }
 
 run_Test(){
@@ -38,50 +54,38 @@ run_Test(){
     QUERYFILE="${DATASETS}/$4"
 	#test configs
     RESULTSFOLDER="TEST-$2_I-$3_Q-$4_IT-${ITERATIONS}"
-	CLIENT="t5"
-	SERVERS="t6 t7 t8"
-	BRANCH="Tests"
 
-    #Setup
-	echo "--- Setup client ${CLIENT} ---"
-	change_branch $CLIENT $BRANCH
-
-	for SERVER in $SERVERS
+    for TEST in $TESTS
 	do
-		echo "--- Setup server ${SERVER} ---"
-		change_branch "$SERVER" "$BRANCH"
+		#start server
+		for SERVER in $SERVERS
+		do
+			echo "LDLDH-Acc: Starting server at ${SERVER}"
+			run_server_jar "$SERVER" "$CONFIGFILE"
+		done
+
+		ssh $CLIENT "cd ${DIR}; mkdir ${RESULTSFOLDER}"
+
+		#Insert
+		echo "LDLDH-Acc: Runing $TESTS Inserts..."
+		run_test_client_jar "$CLIENT" "$CONFIGFILE" "i" "$INSERTFILE" "$RESULTSFOLDER" "$TEST"
+
+		#Test
+		for IT in $(seq "$ITERATIONS")
+		do
+			#Query
+			sleep 1
+			echo "LDLDH-Acc: Runing $TESTS test ${IT} out of ${ITERATIONS}..."
+			run_test_client_jar "$CLIENT" "$CONFIGFILE" "q" "$QUERYFILE" "$RESULTSFOLDER" "$TEST"
+		done
+
+		#Stop Server
+		for SERVER in $SERVERS
+		do
+			echo "LDLDH-Acc: Stoping server at ${SERVER}..."
+			kill_process "$SERVER"
+		done
 	done
-
-    ### LDLSH ###
-	#start server
-    for SERVER in $SERVERS
-	do
-		echo "LDLDH-Acc: Starting server at ${SERVER}"
-		run_server_jar "$SERVER" "$CONFIGFILE"
-	done
-
-	ssh $CLIENT "cd ${DIR}; mkdir ${RESULTSFOLDER}"
-
-	#Insert
-	echo "LDLDH-Acc: Runing Inserts..."
-    run_test_client_jar "$CLIENT" "$CONFIGFILE" "i" "$INSERTFILE" "$RESULTSFOLDER" "accuracy"
-
-	#Test
-    for IT in $(seq "$ITERATIONS")
-    do
-		#Query
-		sleep 1
-		echo "LDLDH-Acc: Runing test ${IT} out of ${ITERATIONS}..."
-        run_test_client_jar "$CLIENT" "$CONFIGFILE" "q" "$QUERYFILE" "$RESULTSFOLDER" "accuracy"
-    done
-
-	#Stop Server
-    for SERVER in $SERVERS
-	do
-		echo "LDLDH-Acc: Stoping server at ${SERVER}..."
-		kill_process "$SERVER"
-	done
-
 	#Copy results
 	mkdir "${RESULTSFOLDER}"
 	scp -r "${CLIENT}:${DIR}/${RESULTSFOLDER}" "."
@@ -94,6 +98,7 @@ then
    exit 1
 fi
 
+setup_tests
 run_Test "$1" "$2" "$3" "$4"
 
 #if [ "$1" = "--accuracy" ];
