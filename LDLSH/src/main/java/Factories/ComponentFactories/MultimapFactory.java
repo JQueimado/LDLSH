e107babe8f32e6da1c1e03f31dba9b3c1926b641
JQueimado <jpqueimado@gmail.com>
2022-09-12 17:01:43 +0100

    Add SemiPersistentMap to factory

--- LDLSH/src/main/java/Factories/ComponentFactories/MultimapFactory.java
+++ LDLSH/src/main/java/Factories/ComponentFactories/MultimapFactory.java
@@ -4,11 +4,17 @@ import Factories.Factory;
 import SystemLayer.Components.MultiMapImpl.GuavaInMemoryMultiMap;
 import SystemLayer.Components.MultiMapImpl.MultiMap;
 import SystemLayer.Components.MultiMapImpl.RemoteMultimap;
+import SystemLayer.Components.MultiMapImpl.SemiPersistentMultiMap;
 import SystemLayer.Containers.DataContainer;
 
 
 public class MultimapFactory implements Factory {
-    private enum configurations {NONE,GUAVA_MEMORY_MULTIMAP, REMOTE_MULTIMAP}
+    private enum configurations {
+        NONE,
+        GUAVA_MEMORY_MULTIMAP,
+        REMOTE_MULTIMAP,
+        SEMI_PERSISTENT_MULTIMAP
+    }
 
     private DataContainer appContext;
 
@@ -26,6 +32,10 @@ public class MultimapFactory implements Factory {
 
             case REMOTE_MULTIMAP -> {
                 return new RemoteMultimap(appContext);
+            }
+
+            case SEMI_PERSISTENT_MULTIMAP->{
+                return new SemiPersistentMultiMap(appContext);
             }
 
             default ->{
