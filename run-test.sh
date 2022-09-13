1e4128f8327c719d0dc34878916ab3a26dae2e55
JQueimado <jpqueimado@gmail.com>
2022-09-13 13:28:54 +0100

    change tests

--- run-test.sh
+++ run-test.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-./run-test-quinta.sh 100 Split1.txt Split2.txt > log_S1_S2.txt 2>&1;
-./run-test-quinta.sh 100 Split2.txt Split1.txt > log_S2_S1.txt 2>&1;
-./run-test-quinta.sh 100 jqueimado_QS_1000.txt jqueimado_QS_1000.txt > log_jqueimado_QS_1000.txt 2>&1;
-./run-test-quinta.sh 1 SRR75_split2.txt SRR75_split1.txt > log_SRR_25.txt 2>&1;
-./run-test-quinta.sh 1 SRR50_split1.txt SRR50_split2.txt > log_SRR_50.txt 2>&1;
-./run-test-quinta.sh 1 SRR75_split1.txt SRR75_split2.txt > log_SRR_75.txt 2>&1;
+#./run-test-quinta.sh 100 Split1.txt Split2.txt > log_S1_S2.txt 2>&1;
+#./run-test-quinta.sh 100 Split2.txt Split1.txt > log_S2_S1.txt 2>&1;
+#./run-test-quinta.sh 100 jqueimado_QS_1000.txt jqueimado_QS_1000.txt > log_jqueimado_QS_1000.txt 2>&1;
+./run-test-quinta.sh 1 SRR46099591_split1.txt SRR46099591_split2.txt > log_SRR_lim1.txt 2>&1;
+#./run-test-quinta.sh 1 SRR50_split1.txt SRR50_split2.txt > log_SRR_50.txt 2>&1;
+#./run-test-quinta.sh 1 SRR75_split1.txt SRR75_split2.txt > log_SRR_75.txt 2>&1;
