36b5f4d709573b5acbcebe94e4e6fc073ddd00fa
JQueimado <jpqueimado@gmail.com>
2022-09-29 15:04:53 +0100

    Change memory test composition

--- LDLSH/src/main/java/SystemLayer/SystemMain/MemoryTestMain2.java
+++ LDLSH/src/main/java/SystemLayer/SystemMain/MemoryTestMain2.java
@@ -28,8 +28,9 @@ public class MemoryTestMain2 extends SystemMainImp {
     public void run() throws Exception {
         //Commands
         // cmd <file> <lines>
-        String fileName = args[1];
-        Integer lines = Integer.parseInt(args[2]);
+        String op = args[1];
+        String fileName = args[2];
+        Integer lines = Integer.parseInt(args[3]);
 
         //LoadFile
         List<DataObject<String>> data = new ArrayList<>();
@@ -53,77 +54,76 @@ public class MemoryTestMain2 extends SystemMainImp {
         scanner.nextLine();
 
         //Insert File
-        System.out.println("Inserting...");
-        for (DataObject<String> dataElement : data) {
-            //System.out.println("adding:"+ dataElement.getValues());
-            //Execute instruction
-            Timestamp initialTimeStamp = new Timestamp(System.currentTimeMillis());
-            ListenableFuture<DataObject<?>> result = system.insert(dataElement);
-            Futures.addCallback(result, new FutureCallback<>() {
-                final DataObject<?> elem = dataElement;
-                final Timestamp initialTimeStamp1 = initialTimeStamp;
+        switch (op) {
+            case "-i" -> {
+                System.out.println("Inserting...");
+                for (DataObject<String> dataElement : data) {
+                    //System.out.println("adding:"+ dataElement.getValues());
+                    //Execute instruction
+                    Timestamp initialTimeStamp = new Timestamp(System.currentTimeMillis());
+                    ListenableFuture<DataObject<?>> result = system.insert(dataElement);
+                    Futures.addCallback(result, new FutureCallback<>() {
+                        final DataObject<?> elem = dataElement;
+                        final Timestamp initialTimeStamp1 = initialTimeStamp;
 
-                @Override
-                public void onSuccess(DataObject object) {
-                    //Complete
-                    Timestamp finalTimestamp = new Timestamp(System.currentTimeMillis());
-                    int i ;
-                    synchronized (successCounter) {
-                        i = successCounter.getAndIncrement();
-                    }
-                    long totalExecutionTime = finalTimestamp.getTime() - initialTimeStamp1.getTime();
-                    System.out.println(i + "insert for " + elem.getValues() + " execution time: " + totalExecutionTime + " ms");
-                }
+                        @Override
+                        public void onSuccess(DataObject object) {
+                            //Complete
+                            Timestamp finalTimestamp = new Timestamp(System.currentTimeMillis());
+                            int i;
+                            synchronized (successCounter) {
+                                i = successCounter.getAndIncrement();
+                            }
+                            long totalExecutionTime = finalTimestamp.getTime() - initialTimeStamp1.getTime();
+                            System.out.println(i + "insert for " + elem.getValues() + " execution time: " + totalExecutionTime + " ms");
+                        }
 
-                @Override
-                public void onFailure(@NotNull Throwable throwable) {
-                    System.err.println("Insert Failed: " + throwable.getMessage());
-                    throwable.printStackTrace();
+                        @Override
+                        public void onFailure(@NotNull Throwable throwable) {
+                            System.err.println("Insert Failed: " + throwable.getMessage());
+                            throwable.printStackTrace();
+                        }
+                    }, appContext.getCallbackExecutor());
                 }
-            }, appContext.getCallbackExecutor());
+            }
+
+            case "-q" -> {
+                System.out.println("Querying...");
+                for (DataObject<?> dataElement : data) {
+
+                    //Execute Instruction
+                    Timestamp initialTimeStamp = new Timestamp(System.currentTimeMillis());
+                    ListenableFuture<DataObject<?>> result = system.query(dataElement);
+                    Futures.addCallback(result, new FutureCallback<>() {
+                        final DataObject<?> elem = dataElement;
+                        final Timestamp initialTimeStamp1 = initialTimeStamp;
+
+                        @Override
+                        public void onSuccess(DataObject object) {
+                            //Complete
+                            Timestamp finalTimestamp = new Timestamp(System.currentTimeMillis());
+                            int n;
+                            synchronized (successCounter) {
+                                n = successCounter.getAndIncrement();
+                            }
+                            long totalExecutionTime = finalTimestamp.getTime() - initialTimeStamp1.getTime();
+                            System.out.println(n + " - query for " + elem.getValues() +
+                                    " returned: " + object.getValues() +
+                                    "execution time: " + totalExecutionTime + " ms");
+                        }
+
+                        @Override
+                        public void onFailure(@NotNull Throwable throwable) {
+                            //Completed with error
+                        }
+                    }, appContext.getCallbackExecutor());
+                }
+            }
         }
-        system.stop();
-        System.out.println("Done. Please Check memory.");
-        scanner.nextLine();
-
-        system = new SystemImpl(appContext);
-
-        successCounter.set(0);
-
-        System.out.println("Querying...");
-        for (DataObject<?> dataElement : data){
-
-            //Execute Instruction
-            Timestamp initialTimeStamp = new Timestamp(System.currentTimeMillis());
-            ListenableFuture<DataObject<?>> result = system.query(dataElement);
-            Futures.addCallback(result, new FutureCallback<>() {
-                final DataObject<?> elem = dataElement;
-                final Timestamp initialTimeStamp1 = initialTimeStamp;
-                @Override
-                public void onSuccess(DataObject object) {
-                    //Complete
-                    Timestamp finalTimestamp = new Timestamp(System.currentTimeMillis());
-                    int n;
-                    synchronized (successCounter) {
-                        n = successCounter.getAndIncrement();
-                    }
-                    long totalExecutionTime = finalTimestamp.getTime() - initialTimeStamp1.getTime();
-                    System.out.println(n + " - query for "+ elem.getValues() +
-                            " returned: " + object.getValues() +
-                            "execution time: "+ totalExecutionTime+" ms");
-                }
-
-                @Override
-                public void onFailure(@NotNull Throwable throwable) {
-                    //Completed with error
-                }
-            }, appContext.getCallbackExecutor());
-        }
-        System.out.println("Done. Please Check memory.");
-        scanner.nextLine();
-
         //Shutdown
         system.stop();
+        System.out.println("Done. Please Check memory.");
+        scanner.nextLine();
         System.exit(0);
 
     }
