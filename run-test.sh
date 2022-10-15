e00175aec1dcd57f70286d5dafbe607ecca278d7
JQueimado <jpqueimado@gmail.com>
2022-10-15 14:04:09 +0100

    fix log name

--- run-test.sh
+++ run-test.sh
@@ -5,4 +5,4 @@ rm -rf TESTS* log_*
 
 ./run-test-quinta.sh . 10 SRR_2400000_aa SRR_2400000_aa > log_SRRaa_SRRaa.txt 2>&1;
 ./run-test-quinta.sh . 10 SRR_2400000_aa SRR_2400000_ab > log_SRRaa_SRRab.txt 2>&1;
-./run-test-quinta.sh . 10 SRR_2400000_aa SRR_2400000_aw > log_SRRaa_SRRac.txt 2>&1;
+./run-test-quinta.sh . 10 SRR_2400000_aa SRR_2400000_aw > log_SRRaa_SRRaw.txt 2>&1;
