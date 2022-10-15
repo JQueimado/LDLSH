f3e8953f7f351868fe56afd2923f87ad0d1d0f78
JQueimado <jpqueimado@gmail.com>
2022-10-15 14:05:23 +0100

    rename datasets

--- run-test.sh
+++ run-test.sh
@@ -3,6 +3,6 @@
 ./run-quinta-servers.sh all -k
 rm -rf TESTS* log_*
 
-./run-test-quinta.sh . 10 SRR_2400000_aa SRR_2400000_aa > log_SRRaa_SRRaa.txt 2>&1;
-./run-test-quinta.sh . 10 SRR_2400000_aa SRR_2400000_ab > log_SRRaa_SRRab.txt 2>&1;
-./run-test-quinta.sh . 10 SRR_2400000_aa SRR_2400000_aw > log_SRRaa_SRRaw.txt 2>&1;
+./run-test-quinta.sh . 10 SRR2400000aa SRR2400000aa > log_SRRaa_SRRaa.txt 2>&1;
+./run-test-quinta.sh . 10 SRR2400000aa SRR2400000ab > log_SRRaa_SRRab.txt 2>&1;
+./run-test-quinta.sh . 10 SRR2400000aa SRR2400000aw > log_SRRaa_SRRaw.txt 2>&1;
