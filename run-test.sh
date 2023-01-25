f994db08481249c5aee92e689eca96ddefa1116e
JQueimado <jpqueimado@gmail.com>
2023-01-25 13:18:12 +0000

    change tests

--- run-test.sh
+++ run-test.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
-
 ./run-quinta-servers.sh all -k
-rm -rf TESTS* log_*
+./run-test-quinta.sh . 10 SRR10000shufaa SRR10000shufaa > log_SRRaa_SRRaa_100.txt 2>&1;
+./run-test-quinta.sh . 10 SRR10000shufaa SRR10000shufab > log_SRRaa_SRRab_100.txt 2>&1;
+./run-test-quinta.sh . 10 SRR10000shufaa SRR10000shufac > log_SRRaa_SRRaw_100.txt 2>&1;
 
-./run-test-quinta.sh . 10 SRR2400000aa SRR2400000aa > log_SRRaa_SRRaa.txt 2>&1;
-./run-test-quinta.sh . 10 SRR2400000aa SRR2400000ab > log_SRRaa_SRRab.txt 2>&1;
-./run-test-quinta.sh . 10 SRR2400000aa SRR2400000aw > log_SRRaa_SRRaw.txt 2>&1;
+./run-test-quinta.sh . 10 SRR100000shufaa SRR100000shufaa > log_SRRaa_SRRaa_100.txt 2>&1;
+./run-test-quinta.sh . 10 SRR100000shufaa SRR100000shufab > log_SRRaa_SRRab_100.txt 2>&1;
+./run-test-quinta.sh . 10 SRR100000shufaa SRR100000shufac > log_SRRaa_SRRaw_100.txt 2>&1;
