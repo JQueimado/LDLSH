3b8171ffc63196803452583fc04194496f1d2bf3
JQueimado <jpqueimado@gmail.com>
2023-01-27 15:23:29 +0000

    change file names

--- run-test-2.sh
+++ run-test-2.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 ./run-quinta-servers.sh all -k
-./run-test-quinta.sh . 10 SRR10000shufaa_x10.txt SRR10000shufaa_x10.txt > log_SRRaa_SRRaa_10x10.txt 2>&1;
-./run-test-quinta.sh . 10 SRR10000shufaa_x10.txt SRR10000shufab_x10.txt > log_SRRaa_SRRab_10x10.txt 2>&1;
-./run-test-quinta.sh . 10 SRR10000shufaa_x10.txt SRR10000shufac_x10.txt > log_SRRaa_SRRaw_10x10.txt 2>&1;
+./run-test-quinta.sh . 10 SRR100000shufaax10 SRR100000shufaax10 > log_SRRaa_SRRaa_10x10.txt 2>&1;
+./run-test-quinta.sh . 10 SRR100000shufaax10 SRR100000shufabx10 > log_SRRaa_SRRab_10x10.txt 2>&1;
+./run-test-quinta.sh . 10 SRR100000shufaax10 SRR100000shufacx10 > log_SRRaa_SRRaw_10x10.txt 2>&1;
