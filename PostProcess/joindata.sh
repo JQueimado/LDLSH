678c3b6a5e56d388ee8a390927130e6aed2a82f0
JQueimado <jpqueimado@gmail.com>
2023-06-24 17:54:38 +0100

    add latency joins

--- PostProcess/joindata.sh
+++ PostProcess/joindata.sh
@@ -38,3 +38,21 @@ python3 joinData_Normal.py \
     /media/sf_Data/TESTS-t4/TestResults/TestThresholdxAccuracy_10x10a-10x10c.csv \
     /media/sf_Data/TESTS-t4/TestResults/TestThresholdxAccuracy_10x10-10x10.csv \
     3
+
+python3 joinData_Normal.py \
+    /media/sf_Data/TESTS-t4/TestResults/TestThresholdxLatency_10a-10b.csv \
+    /media/sf_Data/TESTS-t4/TestResults/TestThresholdxLatency_10a-10c.csv \
+    /media/sf_Data/TESTS-t4/TestResults/TestThresholdxLatency_10-10.csv \
+    3
+
+python3 joinData_Normal.py \
+    /media/sf_Data/TESTS-t4/TestResults/TestThresholdxLatency_100a-100b.csv \
+    /media/sf_Data/TESTS-t4/TestResults/TestThresholdxLatency_100a-100c.csv \
+    /media/sf_Data/TESTS-t4/TestResults/TestThresholdxLatency_100-100.csv \
+    3
+
+python3 joinData_Normal.py \
+    /media/sf_Data/TESTS-t4/TestResults/TestThresholdxLatency_10x10a-10x10b.csv \
+    /media/sf_Data/TESTS-t4/TestResults/TestThresholdxLatency_10x10a-10x10c.csv \
+    /media/sf_Data/TESTS-t4/TestResults/TestThresholdxLatency_10x10-10x10.csv \
+    3
